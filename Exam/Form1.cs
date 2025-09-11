da84461cbb571729ad865f901b12d86e2fccd7f5
Your Name <your_email@whatever.com>
2025-09-11 17:55:23 +0200

    New Button for test

--- Exam/Form1.cs
+++ Exam/Form1.cs
@@ -6,5 +6,10 @@ namespace Exam
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello from Sasha!");
+        }
     }
 }
