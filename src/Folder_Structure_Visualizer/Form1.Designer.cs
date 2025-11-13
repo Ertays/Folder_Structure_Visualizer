97107ba9a70258e628aef8c5197fad6c21984bab
Your Name <your_email@whatever.com>
2025-11-13 15:41:26 +0100

    Folder_Structure_Visualizer

--- src/Folder_Structure_Visualizer/Form1.Designer.cs
+++ src/Folder_Structure_Visualizer/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Folder_Structure_Visualizer
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Folder_Structure_Visualizer
 {
     partial class Form1
     {
@@ -28,12 +30,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblTitle = new Label();
+            lblCurrentPath = new Label();
+            lblPathValue = new Label();
+            lblFilesTitle = new Label();
+            btnRefresh = new Button();
+            btnDelete = new Button();
+            txtFileList = new TextBox();
+            btnAddFile = new Button();
+            btnAddFolder = new Button();
+            btnChangeDrive = new Button();
+            btnRename = new Button();
+            lblDriveInfo = new Label();
+            SuspendLayout();
+
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            lblTitle.Location = new Point(20, 17);
+            lblTitle.Margin = new Padding(5, 0, 5, 0);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(376, 32);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Folder Structure Visualizer";
+
+            lblCurrentPath.AutoSize = true;
+            lblCurrentPath.Location = new Point(20, 87);
+            lblCurrentPath.Margin = new Padding(5, 0, 5, 0);
+            lblCurrentPath.Name = "lblCurrentPath";
+            lblCurrentPath.Size = new Size(113, 25);
+            lblCurrentPath.TabIndex = 1;
+            lblCurrentPath.Text = "Current Path:";
+
+            lblPathValue.AutoSize = true;
+            lblPathValue.Location = new Point(145, 87);
+            lblPathValue.Margin = new Padding(5, 0, 5, 0);
+            lblPathValue.Name = "lblPathValue";
+            lblPathValue.Size = new Size(34, 25);
+            lblPathValue.TabIndex = 2;
+            lblPathValue.Text = "C:\\";
+
+            lblFilesTitle.AutoSize = true;
+            lblFilesTitle.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold);
+            lblFilesTitle.Location = new Point(20, 163);
+            lblFilesTitle.Margin = new Padding(5, 0, 5, 0);
+            lblFilesTitle.Name = "lblFilesTitle";
+            lblFilesTitle.Size = new Size(65, 25);
+            lblFilesTitle.TabIndex = 4;
+            lblFilesTitle.Text = "Files:";
+
+            btnRefresh.Location = new Point(20, 673);
+            btnRefresh.Margin = new Padding(5, 6, 5, 6);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(125, 44);
+            btnRefresh.TabIndex = 5;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += btnRefresh_Click;
+
+            btnDelete.Location = new Point(475, 673);
+            btnDelete.Margin = new Padding(5, 6, 5, 6);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(125, 44);
+            btnDelete.TabIndex = 6;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+
+            txtFileList.Location = new Point(20, 202);
+            txtFileList.Margin = new Padding(5, 6, 5, 6);
+            txtFileList.Multiline = true;
+            txtFileList.Name = "txtFileList";
+            txtFileList.ReadOnly = true;
+            txtFileList.ScrollBars = ScrollBars.Vertical;
+            txtFileList.Size = new Size(764, 439);
+            txtFileList.TabIndex = 7;
+
+            btnAddFile.Location = new Point(20, 731);
+            btnAddFile.Margin = new Padding(5, 6, 5, 6);
+            btnAddFile.Name = "btnAddFile";
+            btnAddFile.Size = new Size(125, 44);
+            btnAddFile.TabIndex = 8;
+            btnAddFile.Text = "Add File";
+            btnAddFile.UseVisualStyleBackColor = true;
+            btnAddFile.Click += btnAddFile_Click;
+
+            btnAddFolder.Location = new Point(155, 731);
+            btnAddFolder.Margin = new Padding(5, 6, 5, 6);
+            btnAddFolder.Name = "btnAddFolder";
+            btnAddFolder.Size = new Size(133, 44);
+            btnAddFolder.TabIndex = 9;
+            btnAddFolder.Text = "Add Folder";
+            btnAddFolder.UseVisualStyleBackColor = true;
+            btnAddFolder.Click += btnAddFolder_Click;
+
+            btnChangeDrive.Location = new Point(298, 731);
+            btnChangeDrive.Margin = new Padding(5, 6, 5, 6);
+            btnChangeDrive.Name = "btnChangeDrive";
+            btnChangeDrive.Size = new Size(167, 44);
+            btnChangeDrive.TabIndex = 12;
+            btnChangeDrive.Text = "Change Drive";
+            btnChangeDrive.UseVisualStyleBackColor = true;
+            btnChangeDrive.Click += btnChangeDrive_Click;
+
+            btnRename.Location = new Point(475, 731);
+            btnRename.Margin = new Padding(5, 6, 5, 6);
+            btnRename.Name = "btnRename";
+            btnRename.Size = new Size(125, 44);
+            btnRename.TabIndex = 13;
+            btnRename.Text = "Rename";
+            btnRename.UseVisualStyleBackColor = true;
+            btnRename.Click += btnRename_Click;
+
+            lblDriveInfo.AutoSize = true;
+            lblDriveInfo.Location = new Point(20, 125);
+            lblDriveInfo.Margin = new Padding(5, 0, 5, 0);
+            lblDriveInfo.Name = "lblDriveInfo";
+            lblDriveInfo.Size = new Size(61, 25);
+            lblDriveInfo.TabIndex = 3;
+            lblDriveInfo.Text = "Drives";
+
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(807, 785);
+            Controls.Add(btnRename);
+            Controls.Add(btnChangeDrive);
+            Controls.Add(btnAddFolder);
+            Controls.Add(btnAddFile);
+            Controls.Add(txtFileList);
+            Controls.Add(btnDelete);
+            Controls.Add(btnRefresh);
+            Controls.Add(lblFilesTitle);
+            Controls.Add(lblDriveInfo);
+            Controls.Add(lblPathValue);
+            Controls.Add(lblCurrentPath);
+            Controls.Add(lblTitle);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Folder Structure Visualizer";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
+        private Label lblCurrentPath;
+        private Label lblPathValue;
+        private Label lblDriveInfo;
+        private Label lblFilesTitle;
+        private Button btnRefresh;
+        private Button btnDelete;
+        private TextBox txtFileList;
+        private Button btnAddFile;
+        private Button btnAddFolder;
+        private Button btnChangeDrive;
+        private Button btnRename;
     }
 }
--- src/Folder_Structure_Visualizer/Form1.cs
+++ src/Folder_Structure_Visualizer/Form1.cs
@@ -1,10 +1,272 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
 namespace Folder_Structure_Visualizer
 {
     public partial class Form1 : Form
     {
+        string currentFolder = @"C:\";
+        int fileCounter = 1;
+        int folderCounter = 1;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckDrives();
+            LoadFilesAndFolders();
+        }
+
+        private void CheckDrives()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            string driveInfo = "Available drives: ";
+            foreach (DriveInfo drive in allDrives)
+            {
+                if (drive.IsReady)
+                {
+                    driveInfo += drive.Name + " ";
+                }
+            }
+
+            lblPathValue.Text = currentFolder;
+            lblDriveInfo.Text = driveInfo;
+        }
+
+        private void LoadFilesAndFolders()
+        {
+            try
+            {
+                txtFileList.Text = "";
+
+                if (!Directory.Exists(currentFolder))
+                {
+                    txtFileList.Text = "Drive not available";
+                    return;
+                }
+
+                string[] allFolders = Directory.GetDirectories(currentFolder);
+                string[] allFiles = Directory.GetFiles(currentFolder);
+
+                UpdateCounters(allFiles, allFolders);
+
+                foreach (string folderPath in allFolders)
+                {
+                    string folderName = Path.GetFileName(folderPath);
+                    txtFileList.Text += "[FOLDER] " + folderName + "\r\n";
+                }
+
+                foreach (string filePath in allFiles)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    txtFileList.Text += "[FILE] " + fileName + "\r\n";
+                }
+
+                txtFileList.Text += $"\r\nTotal: {allFolders.Length} folders, {allFiles.Length} files";
+
+            }
+            catch (Exception error)
+            {
+                txtFileList.Text = "Error: " + error.Message;
+            }
+        }
+
+        private void UpdateCounters(string[] files, string[] folders)
+        {
+            fileCounter = 1;
+            folderCounter = 1;
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                if (fileName.StartsWith("NewFile") || fileName.StartsWith("MyFile"))
+                {
+                    string numberPart = fileName.Replace("NewFile", "").Replace("MyFile", "").Replace(".txt", "");
+                    if (int.TryParse(numberPart, out int num))
+                    {
+                        if (num >= fileCounter) fileCounter = num + 1;
+                    }
+                }
+            }
+
+            foreach (string folder in folders)
+            {
+                string folderName = Path.GetFileName(folder);
+                if (folderName.StartsWith("NewFolder") || folderName.StartsWith("MyFolder"))
+                {
+                    string numberPart = folderName.Replace("NewFolder", "").Replace("MyFolder", "");
+                    if (int.TryParse(numberPart, out int num))
+                    {
+                        if (num >= folderCounter) folderCounter = num + 1;
+                    }
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadFilesAndFolders();
+        }
+
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string newFileName = $"MyFile{fileCounter}.txt";
+                string newFilePath = Path.Combine(currentFolder, newFileName);
+
+                File.WriteAllText(newFilePath, $"This is my new file number {fileCounter}");
+                MessageBox.Show($"File added: {newFileName}");
+
+                fileCounter++;
+                LoadFilesAndFolders();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+        }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string newFolderName = $"MyFolder{folderCounter}";
+                string newFolderPath = Path.Combine(currentFolder, newFolderName);
+
+                Directory.CreateDirectory(newFolderPath);
+                MessageBox.Show($"Folder added: {newFolderName}");
+
+                folderCounter++;
+                LoadFilesAndFolders();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+        }
+
+        private void btnChangeDrive_Click(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            if (currentFolder == @"C:\")
+            {
+                foreach (DriveInfo drive in allDrives)
+                {
+                    if (drive.IsReady && drive.Name != @"C:\")
+                    {
+                        currentFolder = drive.Name;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                currentFolder = @"C:\";
+            }
+
+            fileCounter = 1;
+            folderCounter = 1;
+
+            lblPathValue.Text = currentFolder;
+            LoadFilesAndFolders();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string itemName = Microsoft.VisualBasic.Interaction.InputBox(
+                    "Enter file or folder name to delete:",
+                    "Delete",
+                    "");
+
+                if (string.IsNullOrEmpty(itemName))
+                {
+                    return;
+                }
+
+                string fullPath = Path.Combine(currentFolder, itemName);
+
+                if (File.Exists(fullPath))
+                {
+                    File.Delete(fullPath);
+                    MessageBox.Show("File deleted: " + itemName);
+                }
+                else if (Directory.Exists(fullPath))
+                {
+                    Directory.Delete(fullPath, true);
+                    MessageBox.Show("Folder deleted: " + itemName);
+                }
+                else
+                {
+                    MessageBox.Show("File or folder not found: " + itemName);
+                    return;
+                }
+
+                LoadFilesAndFolders();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string oldName = Microsoft.VisualBasic.Interaction.InputBox(
+                    "Enter current file or folder name:",
+                    "Rename",
+                    "");
+
+                if (string.IsNullOrEmpty(oldName))
+                {
+                    return;
+                }
+
+                string newName = Microsoft.VisualBasic.Interaction.InputBox(
+                    "Enter new name:",
+                    "Rename",
+                    "");
+
+                if (string.IsNullOrEmpty(newName))
+                {
+                    return;
+                }
+
+                string oldPath = Path.Combine(currentFolder, oldName);
+                string newPath = Path.Combine(currentFolder, newName);
+
+                if (File.Exists(oldPath))
+                {
+                    File.Move(oldPath, newPath);
+                    MessageBox.Show("File renamed to: " + newName);
+                }
+                else if (Directory.Exists(oldPath))
+                {
+                    Directory.Move(oldPath, newPath);
+                    MessageBox.Show("Folder renamed to: " + newName);
+                }
+                else
+                {
+                    MessageBox.Show("File or folder not found: " + oldName);
+                    return;
+                }
+
+                LoadFilesAndFolders();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
         }
     }
 }
